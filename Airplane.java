da94a76b31d355472c31c0fb0eef5756174147db
thaddeus <thaddeus@sjccs142.saintjoe.edu>
2015-02-16 13:39:17 -0500

    comment for the new thing that i dont care about

--- Airplane.java
+++ Airplane.java
@@ -1,16 +1,62 @@
+// Coded by Thaddeus Albert
+// Airplane problem givin in class to understand constructors
+// Started on 2/11/15
+
 public class Airplane {
 	public String make;
 	public String model;
-	public int year; 
+	public int year;
 	public int capacity;
-/*---------------------------------------------------------------------------*/
+
+// -----------------------------------
 
 	Airplane(String thisMake, String thisModel, int thisYear, int thisCapacity) {
-	make = thisMake;
-	model = thisModel;
-	year = thisYear;
-	capacity = thisCapacity;
- 	}
+	  make = thisMake;
+	  model = thisModel;
+	  year = thisYear;
+	  capacity = thisCapacity;
+	}
+
+Airplane(String thisMake, String thisModel, int thisYear) {
+	
+	  make = thisMake;
+	  model = thisModel;
+	  year = thisYear;
+	}
+
+	Airplane(String thisMake, String thisModel) {
+
+	  make = thisMake;
+	  model = thisModel;
+	}
+
+	Airplane(String thisMake) {
+	
+	  make = thisMake;
+	} 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
