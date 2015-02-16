b23ad0d4dfba0257e78306b508ec7572d5f181fe
thaddeus <thaddeus@sjccs142.saintjoe.edu>
2015-02-16 13:38:19 -0500

    new comment for Airplane driver class

--- AirplaneDriver.java
+++ AirplaneDriver.java
@@ -1,6 +1,15 @@
+// Coded by Thaddeus Albert
+// Driver test file for the Airplane Class
+// Started on 2/1/15
+
 public class AirplaneDriver {
 	public static void main(String[] args) {
 	  // Now create an airplane
 	  Airplane myplane = new Airplane("Make","Model",1999,100);
 	}
+	
 }
+
+
+
+
